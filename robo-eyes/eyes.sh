a259427933a1ba003642d38882c85f0d6d5d2a2e
Ethan Jahn <ethanjahn@gmail.com>
2020-12-05 19:08:48 -0600

    Combined arm and eyes into single docker-compose, updated arm code to ROS melodic

--- robo-eyes/eyes.sh
+++ robo-eyes/eyes.sh
@@ -1,1 +1,9 @@
+# wait for roscore to be running
+./wait-for-it.sh 127.0.0.1:11311
+
+# source
+source /opt/ros/melodic/setup.bash
+source /home/developer/catkin_ws/devel/setup.bash
+
+# launch ros driver
 roslaunch azure_kinect_ros_driver driver.launch
